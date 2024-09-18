360448afcb038471689f409ffd0ab843a11217fe
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-09-17 20:21:14 -0400

    added an update route

---
+++ server/routes/api/user-routes.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { User } from '../../models/index.js';
+
+
+const router = express.Router();
+
+
+router.put('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { username, password } = req.body;
+    try {
+      const user = await User.findByPk(id);
+      if (user) {
+        user.username = username;
+        user.password = password;
+        await user.save();
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
+    } catch (error: any) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+
+export default router;
