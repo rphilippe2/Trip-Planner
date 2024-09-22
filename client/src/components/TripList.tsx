81c2ad395c6d4ad8e41019e4a5a50360f7df46ea
AdminChatter <xiangzhenwuyu@gmail.com>
2024-09-22 16:35:05 -0400

    removed react in tripList

--- client/src/components/TripList.tsx
+++ client/src/components/TripList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import type { Trail } from '../interfaces/Trip-Planner';
 import Backpack from './Backpack';
