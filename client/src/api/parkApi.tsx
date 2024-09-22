cf4d8eb937ec85ac916bfe09cc2ef5498d1b371f
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-09-21 23:23:55 -0400

    mmm

--- client/src/api/parkApi.tsx
+++ client/src/api/parkApi.tsx
@@ -1,41 +1,48 @@
-
-  // Fetch parks based on user input
-  const fetchParkByName = async (parkName: string): Promise<void> => {
-    const apiKey = process.env.API_KEY_PARKS;
-    try {
-      const response = await fetch(
-        `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&q=${parkName}`
-      );
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
+// Fetch parks based on user input
+const fetchParkByName = async (parkName: string): Promise<void> => {
+  const apiKey = process.env.API_KEY_PARKS;
+  try {
+    const response = await fetch(
+      `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&q=${parkName}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
+    );
 
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      console.log("Error from data retrieval:", err);
-    }
-  };
-
-  const fetchParkByCityName = async (cityName: string): Promise<void> => {
-    const apiKey = process.env.API_KEY_PARKS;
-    try {
-      const response = await fetch(
-        `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&q=${cityName}`
-      );
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      console.log("Error from data retrieval:", err);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
 
-  };
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log("Error from data retrieval:", err);
+  }
+};
 
+const fetchParkByCityName = async (cityName: string): Promise<void> => {
+  const apiKey = process.env.API_KEY_PARKS;
+  try {
+    const response = await fetch(
+      `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}&q=${cityName}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log("Error from data retrieval:", err);
+  }
+};
 
 export { fetchParkByName, fetchParkByCityName };
