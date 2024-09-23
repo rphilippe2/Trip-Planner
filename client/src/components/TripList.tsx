9b4337561c1423abfd730ef128b086ba86740544
moayed10111 <58371121+moayed10111@users.noreply.github.com>
2024-09-23 15:28:30 -0400

    ddd

--- client/src/components/TripList.tsx
+++ client/src/components/TripList.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import type { Trail } from '../interfaces/Trail';
-import Backpack from './Backpack';
+import Backpack from './backpack';
 import { Item } from '../interfaces/Item';
 
 
